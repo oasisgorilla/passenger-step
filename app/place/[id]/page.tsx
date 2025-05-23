dbeab791c2934a6607a6f6efe2dd43d5fe0abb1b
oasisgorilla <yongjae0522@gmail.com>
2025-05-23 13:31:49 +0900

    type 오류 해결

--- app/place/[id]/page.tsx
+++ app/place/[id]/page.tsx
@@ -1,7 +1,13 @@
 import { destinations } from "@/lib/data"
 import PlaceModal from "@/components/place-modal"
 
-export default async function Page({ params }: { params: { id: string } }) {
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function Page({ params }: PageProps) {
   const destination = destinations.find((d) => d.id === params.id)
   if (!destination) return null
 
