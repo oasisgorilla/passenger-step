b25a6ced54d6740db39ae88e34b5c1c85b8d55a1
oasisgorilla <yongjae0522@gmail.com>
2025-05-26 10:57:40 +0900

    params 비동기 처리

--- app/place/[id]/page.tsx
+++ app/place/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { destinations } from "@/lib/data"
 import PlaceModal from "@/components/place-modal"
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const destination = destinations.find((d) => d.id === params.id)
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const destination = destinations.find((d) => d.id === id)
   if (!destination) return null
 
   return <PlaceModal destination={destination} />
